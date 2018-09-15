0dc137774d280e4a4e17aaf8bfb847bc6bb96b53
Unknown <yassinefikrat@gmail.com>
2018-09-15 13:57:02 +0200

    Pretty .vue

--- src/components/ChuccButton.vue
+++ src/components/ChuccButton.vue
@@ -17,32 +17,30 @@ export default {
 </script>
 
 <style scoped>
+* {
+  font-family: 'Hack', monospace;
+}
 
-  * {
-    font-family: 'Hack', monospace;
-  }
+button {
+  cursor: pointer;
+  background-color: #ffcad4;
+  border: none;
+  border-radius: 5px;
+  color: #9d8189;
+  padding: 10px 40px;
+  display: inline-block;
+  font-size: 16px;
+  box-shadow: 0 9px #9d8189;
+  outline: none;
+}
 
-  button {
-    cursor: pointer;
-    background-color: #FFCAD4;
-    border: none;
-    border-radius: 5px;
-    color: #9D8189;
-    padding: 10px 40px;
-    display: inline-block;
-    font-size: 16px;
-    box-shadow: 0 9px #9D8189;
-    outline: none;
-  }
+button:hover {
+  background-color: #f4acb7;
+}
 
-  button:hover {
-    background-color: #F4ACB7;
-  }
-
-  button:active {
-    background-color: #F4ACB7;
-    box-shadow: 0 5px #9D8189;
-    transform: translateY(4px);
-  }
-
+button:active {
+  background-color: #f4acb7;
+  box-shadow: 0 5px #9d8189;
+  transform: translateY(4px);
+}
 </style>
